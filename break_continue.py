314606435b4b521a78348d5fd68adeb9940cc80b
Eduard Nicolas Sarmiento Herrera <edunsarmiento@soy.sena.edu.co>
2022-09-30 00:19:00 -0500

    se adjuntan ejercicios con ciclos for y while, break y continue, el archivo de break_continue.py no ejecuta el segundo caso de for - pendiente de revisar

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+def run():
+    for i in range(10000):
+        if i == 5678:
+            break
+
+    # for contador in range (1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+
+if __name__ == '__main__':
+    run()
