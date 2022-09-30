a5e9d19cb6f48ff0934d4c06b184349daa08b50a
Eduard Nicolas Sarmiento Herrera <edunsarmiento@soy.sena.edu.co>
2022-09-30 00:46:24 -0500

    se agrega prueba de primalidad

--- break_continue.py
+++ break_continue.py
@@ -1,12 +1,20 @@
 def run():
-    for i in range(10000):
-        if i == 5678:
-            break
+    texto = input ('Escribe un texto: ')
+    for letra in texto:
+        if letra == 'o':
+            break   
+        print(letra)        
+
+    # for i in range(10000):
+    #     if i == 5678:
+    #         break
 
     # for contador in range (1000):
     #     if contador % 2 != 0:
     #         continue
     #     print(contador)
+
+
 
 if __name__ == '__main__':
     run()
