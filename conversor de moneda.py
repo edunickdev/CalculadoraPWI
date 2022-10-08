d828cce8e5b8a808a15f2eef177a8a05a65d9b61
edunickdev <114629663+edunickdev@users.noreply.github.com>
2022-10-07 22:48:42 -0500

    mejorando el conversor de monedad

--- conversor de moneda.py
+++ conversor de moneda.py
@@ -15,11 +15,48 @@ def conversordeDolar(tipo_moneda, cantDolares):
     calculo = str(cantidad * cantDolares)
     cambio = round(float(calculo), 2)
 
+
 ######### FIN FUNCIONES ###############
 
+
+######### INICIO MENU con match y cases python 3.10 ###############
+
+tipoConversion = int(input("""¿Que tipo de cambio desea realizar?, digíte la opción de su preferencia:
+                                    1. de su moneda local a Dolar.
+                                    2. de dolar a su moneda local.
+                De momento se dispone de cambio de las siguientes monedas (Peso Colombiano, Peso Argentino, Peso Mexicano y Soles Peruanos.)"""))
+
+
+match tipoConversion:
+    case 1:
+            exchange = int(input("""Elige el tipo de cambio que deseas hacer digitando el número de la opción deseada:
+                                    1. Pesos Colombianos
+                                    2. Pesos Argentinos
+                                    3. Pesos Mexicanos.
+                                    4. Soles Peruanos. \n"""))
+            
+            match exchange:
+                case 1:
+                    conversoraDolar("Colombianos", float(input("Valor del dolar a hoy: ")))
+                case 2:
+                    conversoraDolar("Argentinos", float(input("Valor del dolar a hoy: ")))
+                case 3:
+                    conversoraDolar("Mexicanos", float(input("Valor del dolar a hoy: ")))
+                case 4:
+                    conversoraDolar("Soles Peruanos", float(input("Valor del dolar a hoy: ")))
+                
+    case 2:
+        pass
+    
+######### FIN MENU con match y cases python 3.10 ###############
+
+
+
+
+
 ######### INICIO MENU con If y else y elif ###############
-# menu = """ 
-# Bienvenido al conversor de monedas 
+# menu = """
+# Bienvenido al conversor de monedas
 # 1 - Pesos Colombianos
 # 2 - Pesos Argentinos
 # 3 - Pesos Mexicanos
@@ -39,39 +76,5 @@ def conversordeDolar(tipo_moneda, cantDolares):
 
 ######### FIN MENU ###############
 
-######### INICIO MENU con match y cases python 3.10 ###############
-
-#Menú de opciones
 
 
-match tipoConversion:
-    case 1:
-        conversordeDolar("Colombianos", float(input("Indique a cuanto equivale 1 dolar en la moneda que desea convertir: ")))
-    case 2:
-        conversordeDolar("Argentinos", float(input("Indique a cuanto equivale 1 dolar en la moneda que desea convertir: ")))
-
-tipoConversion = int(input("""¿Que tipo de cambio desea realizar?, digíte la opción de su preferencia:
-                    1. de su moneda local a Dolar.
-                    2. de dolar a su moneda local.
-De momento se dispone de cambio de las siguientes monedas (Peso Colombiano, Peso Argentino, Peso Mexicano y Soles Peruanos.)"""))
-
-
-
-
-exchange = int(input("""Elige el tipo de cambio que deseas hacer digitando el número de la opción deseada:
-                    1. Pesos Colombianos
-                    2. Pesos Argentinos
-                    3. Pesos Mexicanos.
-                    4. Soles Peruanos. \n"""))
-
-match exchange:
-    case 1:
-        conversoraDolar("Colombianos", float(input("Valor del dolar a hoy: ")))
-    case 2:
-        conversoraDolar("Argentinos", float(input("Valor del dolar a hoy: ")))
-    case 3:
-        conversoraDolar("Mexicanos", float(input("Valor del dolar a hoy: ")))
-    case 4:
-        conversoraDolar("Soles Peruanos", float(input("Valor del dolar a hoy: ")))
-
-######### INICIO MENU con match y cases python 3.10 ###############
