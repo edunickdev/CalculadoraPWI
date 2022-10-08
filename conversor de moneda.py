420c045d7dbd62c26b129036341eb5d92ab1ffc1
edunickdev <114629663+edunickdev@users.noreply.github.com>
2022-10-07 20:38:36 -0500

    Ampliando alcance de conversor de moneda

--- conversor de moneda.py
+++ conversor de moneda.py
@@ -1,13 +1,19 @@
 
 ######### INICIO FUNCIONES ###############
 
-def conversor (tipo_moneda, valDolares):
-    cantidad = int(input("Ingrese el dinero en pesos Colombianos, que desea cambiar: "))
+def conversoraDolar (tipo_moneda, valDolares):
+    cantidad = float(input("Ingrese el dinero en su moneda, que desea cambiar: "))
 
     calculo = str(cantidad  / valDolares)
     cambio = round(float(calculo), 2)
 
-    print ("El valor equivalente de su cambio de moneda es: " + str(cambio)) 
+    print ("El valor equivalente de su cambio de moneda es: " + str(cambio))
+
+def conversordeDolar(tipo_moneda, cantDolares):
+    cantidad = float(input("Ingrese la cantidad de Dolares que posee y desea cambiar: "))
+
+    calculo = str(cantidad * cantDolares)
+    cambio = round(float(calculo), 2)
 
 ######### FIN FUNCIONES ###############
 
@@ -36,17 +42,36 @@ def conversor (tipo_moneda, valDolares):
 ######### INICIO MENU con match y cases python 3.10 ###############
 
 #Menú de opciones
+
+
+match tipoConversion:
+    case 1:
+        conversordeDolar("Colombianos", float(input("Indique a cuanto equivale 1 dolar en la moneda que desea convertir: ")))
+    case 2:
+        conversordeDolar("Argentinos", float(input("Indique a cuanto equivale 1 dolar en la moneda que desea convertir: ")))
+
+tipoConversion = int(input("""¿Que tipo de cambio desea realizar?, digíte la opción de su preferencia:
+                    1. de su moneda local a Dolar.
+                    2. de dolar a su moneda local.
+De momento se dispone de cambio de las siguientes monedas (Peso Colombiano, Peso Argentino, Peso Mexicano y Soles Peruanos.)"""))
+
+
+
+
 exchange = int(input("""Elige el tipo de cambio que deseas hacer digitando el número de la opción deseada:
                     1. Pesos Colombianos
                     2. Pesos Argentinos
-                    3. Pesos Mexicanos.\n"""))
+                    3. Pesos Mexicanos.
+                    4. Soles Peruanos. \n"""))
 
 match exchange:
     case 1:
-        conversor("Colombianos", float(input("Valor del dolar a hoy: ")))
+        conversoraDolar("Colombianos", float(input("Valor del dolar a hoy: ")))
     case 2:
-        conversor("Argentinos", float(input("Valor del dolar a hoy: ")))
+        conversoraDolar("Argentinos", float(input("Valor del dolar a hoy: ")))
     case 3:
-        conversor("Mexic", float(input("Valor del dolar a hoy: ")))
+        conversoraDolar("Mexicanos", float(input("Valor del dolar a hoy: ")))
+    case 4:
+        conversoraDolar("Soles Peruanos", float(input("Valor del dolar a hoy: ")))
 
 ######### INICIO MENU con match y cases python 3.10 ###############
